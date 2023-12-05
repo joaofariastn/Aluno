5afac41e3cda7fa53e9dc0bcf866ff64440994b7
joaofariastn <126178518+joaofariastn@users.noreply.github.com>
2023-12-05 20:25:51 -0300

    Alterado

--- src/App.java
+++ src/App.java
@@ -7,14 +7,14 @@ public class App {
     public static void main(String[] args) {
         iAlunoRepository alunoRepository = new ArrayListAlunoRepository();
 
-        // Adicionar 5 alunos a lista
+        // Adicionar os Alunos
         alunoRepository.adicionar(new Aluno("João", "0000000001")); 
         alunoRepository.adicionar(new Aluno("Toin", "0000000002"));
         alunoRepository.adicionar(new Aluno("Fabio", "0000000003"));
         alunoRepository.adicionar(new Aluno("Manoel", "0000000004")); 
         alunoRepository.adicionar(new Aluno("Alan", "0000000005"));
         
-        // Listar os alunos
+      
         for (Aluno aluno : alunoRepository.listar()) {
             System.out.println("Nome: " + aluno.getNome() + ", Matrícula: " + aluno.getMatricula());
         }
@@ -24,13 +24,13 @@ public class App {
             Aluno alunoBuscado = alunoRepository.buscar("Fabio");
             System.out.println("Aluno buscado: " + alunoBuscado.getNome());
 
-            // Remover um aluno
+            
             alunoRepository.remover(alunoBuscado);
         } catch (AlunoNaoEncontradoException e) {
             System.out.println(e.getMessage());
         }
 
-        // Listar os alunos restantes
+        
         for (Aluno aluno : alunoRepository.listar()) {
             System.out.println("Nome: " + aluno.getNome() + ", Matrícula: " + aluno.getMatricula());
         }
--- src/contracts/iAlunoRepository.java
+++ src/contracts/iAlunoRepository.java
@@ -4,8 +4,8 @@ import java.util.List;
 import models.Aluno;
 
 public interface iAlunoRepository {
-    void adicionar(Aluno aluno); // Adicionar um aluno
-    void remover(Aluno aluno); // Remove um aluno
-    Aluno buscar(String nome); // Busca o aluno pelo nome e retorna o Aluno
-    List<Aluno> listar(); // Lista os alunos adicionado
+    void adicionar(Aluno aluno); 
+    void remover(Aluno aluno); 
+    Aluno buscar(String nome); 
+    List<Aluno> listar(); 
 }
